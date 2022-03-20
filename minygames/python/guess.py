4889dd89148ce87f85a8bbb46b15d8888594a436
jxah <jxah@example.com>
2022-03-20 19:08:39 -0400

    good

--- minygames/python/guess.py
+++ minygames/python/guess.py
@@ -4,7 +4,7 @@ import random
 # a is my random number
 a = random.randrange(1,10)
 
-responses = ["O. M. G.!", "wow!", "ooh!", "good try!", "almost gone!", "maybe next time. oh and the number is "+str(a)]
+responses = ["O. M. G.!", "wow!","awesome!", "ooh!", "good try!", "almost gone!", "maybe next time. oh and the number is "+str(a)]
 i=0
 #this is for counting times you have guessed
 
