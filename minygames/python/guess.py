537c9f64398862420578296ad1c7329a06acf986
jxah <jxah@example.com>
2022-03-22 18:42:00 -0400

    r

--- minygames/python/guess.py
+++ minygames/python/guess.py
@@ -2,14 +2,19 @@
 import random
 
 # a is my random number
-a = random.randrange(1,10)
+a = 1#random.randrange(1,10)
 
-responses = ["O. M. G.!", "wow!","awesome!", "ooh!", "good try!", "almost gone!", "maybe next time. oh and the number is "+str(a)]
+responses = ["O. M. G.!", "wow!","awesome!", "ooh!", "good!", "almost gone!", "maybe next time. oh and the number is "+str(a)]
 i=0
 #this is for counting times you have guessed
 
 while True:
-  guess = input("Guess my number, which is a number from 1 to 10:")
+  try:
+    guess = input("Guess my number, which is a number from 1 to 10:")
+    int(guess)
+  except:
+    continue
+    
   correct = (a == int(guess))
   strikeout = (i == 6)
   if correct or strikeout: 
